30184003986feca45acb216bf07c3f1e1a64c679
brunoFgR <brunofds281@gmail.com>
2021-08-01 18:45:25 -0300

    feat(src): configure fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
+import Head from 'next/head';
+
 import style from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <h1 className={style.title}>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+
       Hello <span>world</span>!
     </h1>
   )
