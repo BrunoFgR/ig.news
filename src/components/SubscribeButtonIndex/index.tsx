7faeb5efcce2fec135bed057a834a241c9496ea2
brunoFgR <brunofds281@gmail.com>
2021-08-01 23:29:27 -0300

    feat(Home): create Home page

---
+++ src/components/SubscribeButtonIndex/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+export function SubscribeButtonIndex() {
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}>
+        Subscribe now
+      </button>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+import { SubscribeButtonIndex } from '../components/SubscribeButtonIndex';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>world</span>!
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButtonIndex />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
