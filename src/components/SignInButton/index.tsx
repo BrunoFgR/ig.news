19d2a8bc199a098cbc19873ca8c9c853a989df41
brunoFgR <brunofds281@gmail.com>
2021-08-12 10:03:00 -0300

    feat(api/auth): put auth into the project

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,29 @@
-import {FaGithub } from 'react-icons/fa'
-import {FiX } from 'react-icons/fi'
-
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-
-  return isUserLoggedIn ? (
+  const [session] = useSession();
+  
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}>
       <FaGithub color='#04d301'/>
-      Bruno Figueiredo
-      <FiX color='#737300' className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX color='#737300' 
+      className={styles.closeIcon}
+      onClick={() => signOut()}
+      />
     </button>
   ) : (
     <button 
       type="button"
-      className={styles.signInButton}>
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+      >
       <FaGithub color='#eba417'/>
       Sign In with GitHub
     </button>
